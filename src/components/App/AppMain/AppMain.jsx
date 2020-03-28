907f423a45f649b5652e2dcb88f72a6ab61743b6
james@crenexi.com <james@crenexi.com>
2020-03-28 15:42:52 -0700

    AppMain.

---
+++ src/components/App/AppMain/AppMain.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+// import Design from 'components/scenes/Design';
+
+const Home = () => <h1>Hello World</h1>;
+
+const Test = () => (
+  <div>
+    Test
+  </div>
+);
+
+const AppMain = () => {
+  const routes = [
+    { path: '/test', component: Test },
+    // { path: '/design', component: Design },
+  ];
+
+  return (
+    <main>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        {routes.map(({ path, component }) => (
+          <Route key={path} path={path} component={component} />
+        ))}
+        <Redirect to="/" />
+      </Switch>
+    </main>
+  );
+};
+
+export default AppMain;
