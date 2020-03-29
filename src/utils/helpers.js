18cc1f0a425da2e3ced03843a30533dfe96e4f5a
james@crenexi.com <james@crenexi.com>
2020-03-28 20:34:46 -0700

    Removed unnecessary helper.

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,1 @@
-/** Make each prop in object an integer */
-export const entriesToInts = (o) => {
-  Object.entries(o).reduce((a, [k, v]) => ({
-    ...a,
-    [k]: parseInt(v, 10),
-  }), {});
-};
-
-export default {
-  entriesToInts,
-};
+export default {};
