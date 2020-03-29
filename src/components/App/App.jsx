72e4b65d1347bfc9e040958f8985294c1506d260
james@crenexi.com <james@crenexi.com>
2020-03-28 21:31:29 -0700

    Completed Material UI configuration
    Created Material UI configuration for creating a theme. Theme values are imported from SCSS variable map exports. Except for the typography values, which are hardcoded to create the MUI theme, the other values -- colors and breakpoints -- are provided directly from sass exports.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { HashRouter as Router } from 'react-router-dom';
-import faConfig from 'config/font-awesome-config';
+import { ThemeProvider } from '@material-ui/styles';
+import fontAwesomeConfig from 'config/font-awesome-config';
+import muiConfig from 'config/material-ui-config';
 import defaultSettings from 'config/default-settings';
 import { SettingsProvider } from 'contexts/SettingsContext';
 import logger from 'services/logger';
@@ -10,21 +12,24 @@ import AppFooter from 'components/App/AppFooter';
 import './App.scss';
 
 // Initialize Clap2020 icon library
-faConfig.buildLibrary();
+fontAwesomeConfig.buildLibrary();
 
 const App = () => {
   logger.info('\nMain Engine Start.\nT-Zero. SRB Ignition.\nLiftoff.');
+  logger.info(muiConfig.createTheme());
 
   return (
-    <SettingsProvider value={defaultSettings}>
-      <Router>
-        <div styleName="app">
-          <AppHeader />
-          <AppMain />
-          <AppFooter />
-        </div>
-      </Router>
-    </SettingsProvider>
+    <ThemeProvider theme={muiConfig.createTheme()}>
+      <SettingsProvider value={defaultSettings}>
+        <Router>
+          <div styleName="app">
+            <AppHeader />
+            <AppMain />
+            <AppFooter />
+          </div>
+        </Router>
+      </SettingsProvider>
+    </ThemeProvider>
   );
 };
 
