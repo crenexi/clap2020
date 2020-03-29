b1ffb21c93b5d02dfa164c9d2e3080f6127e16ac
james@crenexi.com <james@crenexi.com>
2020-03-28 22:07:56 -0700

    Finished design guide.

--- src/config/theme-config.js
+++ src/config/theme-config.js
@@ -11,6 +11,46 @@ const paletteRows = [
       scssVars.themeCloudLight2,
     ],
   },
+  {
+    title: 'Red Rose',
+    colors: [
+      scssVars.themeRedRoseDark2,
+      scssVars.themeRedRoseDark1,
+      scssVars.themeRedRoseBase,
+      scssVars.themeRedRoseLight1,
+      scssVars.themeRedRoseLight2,
+    ],
+  },
+  {
+    title: 'Blue Rose',
+    colors: [
+      scssVars.themeBlueRoseDark2,
+      scssVars.themeBlueRoseDark1,
+      scssVars.themeBlueRoseBase,
+      scssVars.themeBlueRoseLight1,
+      scssVars.themeBlueRoseLight2,
+    ],
+  },
+  {
+    title: 'Ocean',
+    colors: [
+      scssVars.themeOceanDark2,
+      scssVars.themeOceanDark1,
+      scssVars.themeOceanBase,
+      scssVars.themeOceanLight1,
+      scssVars.themeOceanLight2,
+    ],
+  },
+  {
+    title: 'Marine',
+    colors: [
+      scssVars.themeMarineDark2,
+      scssVars.themeMarineDark1,
+      scssVars.themeMarineBase,
+      scssVars.themeMarineLight1,
+      scssVars.themeMarineLight2,
+    ],
+  },
 ];
 
 const theme = {
