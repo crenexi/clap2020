5b3d235c4cbb2af9f3b5a54b218e3e26a1437018
james@crenexi.com <james@crenexi.com>
2020-03-29 22:06:53 -0700

    Changed browser router type.

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { ThemeProvider } from '@material-ui/styles';
 import fontAwesomeConfig from 'config/font-awesome-config';
 import muiConfig from 'config/material-ui-config';
