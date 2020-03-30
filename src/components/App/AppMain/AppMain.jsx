09844250bcb65711a11724d20fc4f6160110ff1e
james@crenexi.com <james@crenexi.com>
2020-03-30 11:42:42 -0700

    Integrated Google Analytics tracker
    Also edited AppHeader to prepare it to handle path changes and header type changes

--- src/components/App/AppMain/AppMain.jsx
+++ src/components/App/AppMain/AppMain.jsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import Design from 'components/scenes/Design';
+import withAnalyticsTracker from 'utils/with-analytics-tracker';
 
 const Home = () => <h1>Hello World</h1>;
 
 const AppMain = () => {
-  const routes = [
+  const routesData = [
     { path: '/design', component: Design },
   ];
+
+  const routes = routesData.map(({ path, component }) => {
+    const compWithTracker = withAnalyticsTracker(component);
+    return <Route key={path} path={path} component={compWithTracker} />;
+  });
 
   return (
     <main>
       <Switch>
-        <Route exact path="/" component={Home} />
-        {routes.map(({ path, component }) => (
-          <Route key={path} path={path} component={component} />
-        ))}
+        <Route exact path="/" component={withAnalyticsTracker(Home)} />
+        {routes}
         <Redirect to="/" />
       </Switch>
     </main>
