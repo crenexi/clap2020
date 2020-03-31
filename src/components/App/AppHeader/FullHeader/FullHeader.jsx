6a9daa10213d60f752a5ce7dfd4147370b1ff8e0
james@crenexi.com <james@crenexi.com>
2020-03-30 22:41:42 -0700

    Starting dev on header.

--- src/components/App/AppHeader/FullHeader/FullHeader.jsx
+++ src/components/App/AppHeader/FullHeader/FullHeader.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import BgCover from 'components/shared/BgCover';
+import './FullHeader.scss';
 
-const FullHeader = () => (
-  <div>Full Header!!!</div>
-);
+const FullHeader = () => {
+  const assetsPath = process.env.ASSETS_PATH;
+  const coverUrl = `${assetsPath}/covers/nurse.jpg`;
+
+  return (
+    <div styleName="header">
+      <BgCover url={coverUrl} imagePosition="75% top" />
+    </div>
+  );
+};
 
 export default FullHeader;
