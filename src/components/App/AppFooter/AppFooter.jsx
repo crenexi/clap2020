ec020522d4bf3130aec6f0e3c5e55f3756d0dbdb
james@crenexi.com <james@crenexi.com>
2020-03-31 16:19:51 -0700

    Added Logo and date to a basic footer starting point

--- src/components/App/AppFooter/AppFooter.jsx
+++ src/components/App/AppFooter/AppFooter.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
+import BrandLogo from 'components/shared/BrandLogo';
 import './AppFooter.scss';
 
-const AppFooter = () => (
-  <footer styleName="footer">
-    Footer
-  </footer>
-);
+const AppFooter = () => {
+  const clapTime = 'April 23rd @ 7:00 PM';
+
+  return (
+    <footer styleName="footer">
+      <div styleName="footer__main">
+        <div styleName="footer__brand">
+          <BrandLogo variant="white" />
+        </div>
+        <div styleName="footer__clap-time">{clapTime}</div>
+      </div>
+    </footer>
+  );
+};
 
 export default AppFooter;
