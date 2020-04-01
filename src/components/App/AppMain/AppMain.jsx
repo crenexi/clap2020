c1dd1de132c26f48f774c0a7607936fdf99542a5
james@crenexi.com <james@crenexi.com>
2020-04-01 09:23:09 -0700

    Added scenes to router

--- src/components/App/AppMain/AppMain.jsx
+++ src/components/App/AppMain/AppMain.jsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
+import Home from 'components/scenes/Home';
+import Share from 'components/scenes/Share';
+import Latest from 'components/scenes/Latest';
+import Countdown from 'components/scenes/Countdown';
 import Design from 'components/scenes/Design';
 import withAnalyticsTracker from 'utils/with-analytics-tracker';
 
-const Home = () => <div />;
-
 const AppMain = () => {
   const routesData = [
+    { path: '/share', component: Share },
+    { path: '/latest', component: Latest },
+    { path: '/countdown', component: Countdown },
     { path: '/design', component: Design },
   ];
 
