10c53da44836f9af26ad608bbca93a4aa35e44e8
james@crenexi.com <james@crenexi.com>
2020-03-31 19:10:58 -0700

    Header styling.

--- src/components/App/AppHeader/FullHeader/FullHeaderView.jsx
+++ src/components/App/AppHeader/FullHeader/FullHeaderView.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useTheme } from '@material-ui/core/styles';
+import useMediaQuery from '@material-ui/core/useMediaQuery';
 import BgCover from 'components/shared/BgCover';
 import BrandLogo from 'components/shared/BrandLogo';
-import MaskIcon from './MaskIcon';
 import JoinInvite from './JoinInvite';
 import Countdown from './Countdown';
 import IconParade from './IconParade';
 import './FullHeaderView.scss';
 
 const FullHeaderView = ({ coverUrl }) => {
+  const theme = useTheme();
+  const isGtSm = useMediaQuery(theme.breakpoints.up('sm'));
+  const logoVariant = !isGtSm ? 'white' : 'main';
+
   return (
     <div styleName="header">
+      <div styleName="header__logo-aside">
+        <div styleName="header__logo">
+          <BrandLogo variant={logoVariant} />
+        </div>
+      </div>
       <BgCover
         url={coverUrl}
         imagePosition="75% top"
         blanketColor="rgba(255, 255, 255, .65)"
       >
-        <BrandLogo variant="main" />
         <JoinInvite />
-        <MaskIcon />
         <Countdown />
       </BgCover>
       <IconParade />
