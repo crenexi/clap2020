55f297a6473ba44daeabbd03388adca699c431bc
james@crenexi.com <james@crenexi.com>
2020-04-01 21:57:11 -0700

    Misc fixes.

--- src/components/scenes/Home/ThreeSteps/ThreeStepsView.jsx
+++ src/components/scenes/Home/ThreeSteps/ThreeStepsView.jsx
@@ -1,18 +1,23 @@
 import React from 'react';
+import * as Scroll from 'react-scroll';
 import ThreeStepsRow from './ThreeStepsRow';
 import './ThreeStepsView.scss';
 
-const ThreeStepsView = () => (
-  <div styleName="three">
-    <h2>Three Steps...</h2>
-    <div styleName="three__main">
-      <div styleName="three__steps">
-        <ThreeStepsRow />
-        <ThreeStepsRow />
-        <ThreeStepsRow />
+const ThreeStepsView = () => {
+  const ScrollElement = Scroll.Element;
+
+  return (
+    <ScrollElement styleName="three" name="threeSteps">
+      <h2>Three Steps...</h2>
+      <div styleName="three__main">
+        <div styleName="three__steps">
+          <ThreeStepsRow />
+          <ThreeStepsRow />
+          <ThreeStepsRow />
+        </div>
       </div>
-    </div>
-  </div>
-);
+    </ScrollElement>
+  );
+};
 
 export default ThreeStepsView;
