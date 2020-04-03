5cefa965f93f9c1d2988581b79c002cff5cf1c2a
james@crenexi.com <james@crenexi.com>
2020-04-03 10:01:21 -0700

    Added GetReady scene component

---
+++ src/components/scenes/GetReady/GetReady.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './GetReady.scss';
+
+const GetReady = () => (
+  <article styleName="ready">
+    <h1>Get Ready (with BigCountdown)</h1>
+  </article>
+);
+
+export default GetReady;
