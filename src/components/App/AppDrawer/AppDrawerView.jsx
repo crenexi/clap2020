a95088c0e298f4082b30d53a46c87a5510f94ea4
james@crenexi.com <james@crenexi.com>
2020-04-04 11:57:38 -0700

    Added DrawerFAB code

--- src/components/App/AppDrawer/AppDrawerView.jsx
+++ src/components/App/AppDrawer/AppDrawerView.jsx
@@ -1,18 +1,22 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Drawer from '@material-ui/core/Drawer';
+import DrawerFAB from './DrawerFAB';
 import DrawerNav from './DrawerNav';
 import './AppDrawerView.scss';
 
 const AppDrawerView = ({ isOpen, onClose }) => {
   return (
-    <Drawer
-      anchor="bottom"
-      open={isOpen}
-      onClose={onClose}
-    >
-      <DrawerNav />
-    </Drawer>
+    <Fragment>
+      <DrawerFAB isOpen={isOpen} />
+      <Drawer
+        anchor="bottom"
+        open={isOpen}
+        onClose={onClose}
+      >
+        <DrawerNav />
+      </Drawer>
+    </Fragment>
   );
 };
 
--- src/components/App/AppDrawer/DrawerFAB/DrawerFAB.jsx
+++ src/components/App/AppDrawer/DrawerFAB/DrawerFAB.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { Fab as MuiFAB } from '@material-ui/core/Fab';
+import FaIcon from 'components/shared/FaIcon';
 import './DrawerFAB.scss';
 
-const DrawerFAB = () => (
-  <div>Drawer FAB</div>
-);
+const DrawerFAB = ({ drawerIsOpen }) => {
+  const icon = drawerIsOpen ? 'close' : 'menu';
+
+  return (
+    <div>
+      <MuiFAB color="primary" aria-label="add">
+        <FaIcon icon={icon} />
+      </MuiFAB>
+    </div>
+  );
+};
+
+DrawerFAB.propTypes = {
+  drawerIsOpen: PropTypes.bool.isRequired,
+};
 
 export default DrawerFAB;
