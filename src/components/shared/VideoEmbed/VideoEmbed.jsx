2f599acffd6eb229321fec66c15ed439b508905c
james@crenexi.com <james@crenexi.com>
2020-04-06 13:32:48 -0700

    WIP styling bucket videos.

--- src/components/shared/VideoEmbed/VideoEmbed.jsx
+++ src/components/shared/VideoEmbed/VideoEmbed.jsx
@@ -4,27 +4,33 @@ import FaIcon from 'components/shared/FaIcon';
 import './VideoEmbed.scss';
 
 const VideoEmbed = ({ video }) => {
-  // const { videoId, title, attribution, location } = video;
-  const { videoId, title } = video;
+  const { videoId, title, attribution, location } = video;
   const src = `https://www.youtube.com/embed/${videoId}`;
+
+  const iframe = (
+    <iframe
+      styleName="video__iframe"
+      src={src}
+      frameBorder="0"
+      allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
+      allowFullScreen
+      rel="0"
+      title={title}
+    />
+  );
 
   return (
     <article styleName="video ">
       <header styleName="video__header">
         <div styleName="video__icon"><FaIcon icon="video" /></div>
-        <h5>{title}</h5>
+        <div styleName="video__header-content">
+          <h5 styleName="video__header-title">{title}</h5>
+          <div styleName="video__header-details">
+            {location} | By {attribution}
+          </div>
+        </div>
       </header>
-      <div styleName="video__iframe-wrap">
-        <iframe
-          styleName="video__iframe"
-          src={src}
-          frameBorder="0"
-          allow="accelerometer; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-          rel="0"
-          title={title}
-        />
-      </div>
+      <div styleName="video__iframe-wrap">{iframe}</div>
     </article>
   );
 };
