8b0ada8750eae2e06f73a13eeeda07084870ed58
james@crenexi.com <james@crenexi.com>
2020-04-08 15:38:26 -0700

    Added ThankYouInvite instead of ThankYou component

--- src/components/scenes/Home/Home.jsx
+++ src/components/scenes/Home/Home.jsx
@@ -1,20 +1,14 @@
 import React from 'react';
-import useBreakpoint from 'hooks/use-breakpoint';
-import ThankYou from 'components/shared/ThankYou';
 import ThisIsWar from './ThisIsWar';
 import ThreeSteps from './ThreeSteps';
-import './Home.scss';
+import ThankYouInvite from './ThankYouInvite';
 
-const Home = () => {
-  const isGtSm = useBreakpoint('gt-sm');
-
-  return (
-    <article styleName="home">
-      <section><ThreeSteps /></section>
-      <section><ThisIsWar /></section>
-      {isGtSm && <section><ThankYou /></section>}
-    </article>
-  );
-};
+const Home = () => (
+  <article>
+    <section><ThreeSteps /></section>
+    <section><ThisIsWar /></section>
+    <section><ThankYouInvite /></section>
+  </article>
+);
 
 export default Home;
