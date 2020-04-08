390b5481aabf44eaff40b3532963e0ff40942c51
james@crenexi.com <james@crenexi.com>
2020-04-08 11:06:01 -0700

    Button edits to allow using Link conditionally

--- src/components/shared/Button/Button.jsx
+++ src/components/shared/Button/Button.jsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import PropTypes, { oneOf, oneOfType } from 'prop-types';
 import { Button as MuiButton } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import FaIcon from 'components/shared/FaIcon';
 import './Button.scss';
 
 const IconButton = (props) => {
   const { children, variant, size, startIcon, endIcon } = props;
-  const { disabled, click, href, target } = props;
+  const { disabled, click, href, to, target } = props;
 
   // Classes for MUI component
-  const muiClasses = (() => {
+  const classes = (() => {
     const base = 'c2-button';
     return {
       root: `${base} ${base}--${variant}`,
@@ -28,20 +29,52 @@ const IconButton = (props) => {
   const FaStartIcon = !startIcon ? null : <FaIcon icon={startIcon} />;
   const FaEndIcon = !endIcon ? null : <FaIcon icon={endIcon} />;
 
-  return (
+  // Gather props
+  const buttonProps = {
+    classes,
+    size,
+    disabled,
+    startIcon: FaStartIcon,
+    endIcon: FaEndIcon,
+    onClick: click,
+  };
+
+  // If to is supplied, use Link component base.
+  // otherwise supply href and target. For either,
+  // Supply all other props, including onClick
+  return to ? (
     <MuiButton
-      classes={muiClasses}
-      size={size}
-      startIcon={FaStartIcon}
-      endIcon={FaEndIcon}
-      disabled={disabled}
-      onClick={click}
+      to={to}
+      component={Link}
+      {...buttonProps}
+    >
+      {children}
+    </MuiButton>
+  ) : (
+    <MuiButton
       href={href}
       target={target}
+      {...buttonProps}
     >
       {children}
     </MuiButton>
   );
+
+  // <MuiButton
+  //   classes={muiClasses}
+  //   size={size}
+  //   startIcon={FaStartIcon}
+  //   endIcon={FaEndIcon}
+  //   disabled={disabled}
+  //   onClick={click}
+  //   href={href}
+  //   target={target}
+  //   // If to supplies, use Link base
+  //   component={to ? Link : ''}
+  //   to={to}
+  // >
+  //   {children}
+  // </MuiButton>
 };
 
 IconButton.propTypes = {
@@ -54,6 +87,7 @@ IconButton.propTypes = {
   click: oneOfType([() => null, PropTypes.func]),
   href: oneOfType([() => null, PropTypes.string]),
   target: oneOfType([() => null, PropTypes.string]),
+  to: oneOfType([() => null, PropTypes.string]),
 };
 
 IconButton.defaultProps = {
@@ -65,6 +99,7 @@ IconButton.defaultProps = {
   click: null,
   href: null,
   target: null,
+  to: null,
 };
 
 export default IconButton;
--- src/components/shared/IconButton/IconButton.jsx
+++ src/components/shared/IconButton/IconButton.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import PropTypes, { oneOf, oneOfType } from 'prop-types';
 import { IconButton as MuiIconButton } from '@material-ui/core';
+import { Link } from 'react-router-dom';
 import FaIcon from '../FaIcon';
 import './IconButton.scss';
 
 const IconButton = (props) => {
-  const { variant, icon, size, disabled, click, label } = props;
+  const { variant, icon, size, disabled, label } = props;
+  const { click, href, target, to } = props;
+
+  // Class for label
+  const wrapperClass = (() => {
+    const base = 'wrapper__label';
+    return size === 'small' ? `${base} ${base}--small` : base;
+  })();
 
   // Classes for MUI component
-  const muiClasses = (() => {
+  const buttonClasses = (() => {
     const base = 'c2-icon-button';
     return {
       root: `${base} ${base}--${variant}`,
@@ -18,25 +26,55 @@ const IconButton = (props) => {
     };
   })();
 
-  // Class for label
-  const wrapperClass = (() => {
-    const base = 'wrapper__label';
-    return size === 'small' ? `${base} ${base}--small` : base;
-  })();
+  // Gather props
+  const buttonProps = {
+    size,
+    disabled,
+    classes: buttonClasses,
+    onClick: click,
+  };
+
+  // If to is supplied, use Link component base.
+  // otherwise supply href and target. For either,
+  // Supply all other props, including onClick
+  const button = to ? (
+    <MuiIconButton
+      to={to}
+      component={Link}
+      {...buttonProps}
+    >
+      <FaIcon icon={icon} />
+    </MuiIconButton>
+  ) : (
+    <MuiIconButton
+      href={href}
+      target={target}
+      {...buttonProps}
+    >
+      <FaIcon icon={icon} />
+    </MuiIconButton>
+  );
 
   return (
     <div styleName="wrapper">
-      <MuiIconButton
-        classes={muiClasses}
-        size={size}
-        disabled={disabled}
-        onClick={click}
-      >
-        <FaIcon icon={icon} />
-      </MuiIconButton>
+      {button}
       <div styleName={wrapperClass}>{label}</div>
     </div>
   );
+
+  // return (
+  //   <div styleName="wrapper">
+  //     <MuiIconButton
+  //       classes={muiClasses}
+  //       size={size}
+  //       disabled={disabled}
+  //       onClick={click}
+  //     >
+  //       <FaIcon icon={icon} />
+  //     </MuiIconButton>
+  //     <div styleName={wrapperClass}>{label}</div>
+  //   </div>
+  // );
 };
 
 IconButton.propTypes = {
@@ -45,6 +83,9 @@ IconButton.propTypes = {
   size: oneOf(['small', 'medium']),
   disabled: PropTypes.bool,
   click: oneOfType([() => null, PropTypes.func]),
+  href: oneOfType([() => null, PropTypes.string]),
+  target: oneOfType([() => null, PropTypes.string]),
+  to: oneOfType([() => null, PropTypes.string]),
   label: PropTypes.string,
 };
 
@@ -53,6 +94,9 @@ IconButton.defaultProps = {
   size: 'medium',
   disabled: false,
   click: null,
+  href: null,
+  target: null,
+  to: null,
   label: '',
 };
 
