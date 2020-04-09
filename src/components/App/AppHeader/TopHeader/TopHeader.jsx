46707c2f0767753a7ecbaf95841b4c4951f36b6d
james@crenexi.com <james@crenexi.com>
2020-04-09 11:29:41 -0700

    Added button to top header.

--- src/components/App/AppHeader/TopHeader/TopHeader.jsx
+++ src/components/App/AppHeader/TopHeader/TopHeader.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Button from 'components/shared/Button';
 import BrandLogo from 'components/shared/BrandLogo';
 import HeaderFrame from '../HeaderFrame';
 import './TopHeader.scss';
@@ -6,9 +7,17 @@ import './TopHeader.scss';
 const TopHeader = () => (
   <HeaderFrame>
     <div styleName="header">
-      <div styleName="header__logo">
-        <BrandLogo variant="main" />
+      <div styleName="start">
+        <Button to="/" variant="ghost" startIcon="arrow-circle-left">
+          Home
+        </Button>
       </div>
+      <div styleName="center">
+        <div styleName="center__logo">
+          <BrandLogo variant="main" />
+        </div>
+      </div>
+      <div styleName="end" />
     </div>
   </HeaderFrame>
 );
