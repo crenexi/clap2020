fac6127d51bd0771e5081b3df1e1c08d1e148567
james@crenexi.com <james@crenexi.com>
2020-04-12 15:34:41 -0700

    Styling ShareOptions sections.

--- src/components/shared/ShareEditor/ShareOptions/ShareOptions.jsx
+++ src/components/shared/ShareEditor/ShareOptions/ShareOptions.jsx
@@ -13,43 +13,54 @@ const ShareOptions = ({ options, actions }) => {
 
   const sn = {
     options: 'options',
-    item: 'item',
+    section: 'section',
+    sectionHeading: 'section__heading',
+    sectionMain: 'section__main',
+    sectionItem: 'section__item',
   };
 
   // State dropdown
-  const stateSelect = !statesMenu.length ? null : (
-    <div styleName={sn.optionsItem}>
-      <StateSelect
-        menu={statesMenu}
-        value={selectedState}
-        change={changeState}
-      />
-    </div>
+  const stateSelect = (
+    <StateSelect
+      menu={statesMenu}
+      value={selectedState}
+      change={changeState}
+    />
   );
 
   // City dropdown
-  const citySelect = !citiesMenu.length ? null : (
-    <div styleName={sn.optionsItem}>
-      <CitySelect
-        menu={citiesMenu}
-        value={selectedCity}
-        change={changeCity}
-      />
-    </div>
+  const citySelect = (
+    <CitySelect
+      menu={citiesMenu}
+      value={selectedCity}
+      change={changeCity}
+    />
   );
 
   // Poster selection
   const posterSelect = (
-    <div stylename={sn.optionsItem}>
-      <PosterSelect value={selectedPoster} change={changePoster} />
-    </div>
+    <PosterSelect value={selectedPoster} change={changePoster} />
   );
 
   return (
     <div styleName={sn.options}>
-      {stateSelect}
-      {citySelect}
-      {posterSelect}
+      <div styleName={sn.section}>
+        <div styleName={sn.sectionHeading}>Poster Format</div>
+        <div styleName={sn.sectionMain}>
+          <div styleName={sn.sectionItem}>{posterSelect}</div>
+        </div>
+      </div>
+      <div styleName={sn.section}>
+        <div styleName={sn.sectionHeading}>Location Hashtags</div>
+        <div styleName={sn.sectionMain}>
+          <div styleName={sn.sectionItem}>
+            {!statesMenu.length ? null : stateSelect}
+          </div>
+          <div styleName={sn.sectionItem}>
+            {!citiesMenu.length ? null : citySelect}
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
