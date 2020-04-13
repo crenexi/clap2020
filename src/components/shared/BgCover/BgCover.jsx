ed444f1273e9f5788a6f22ff79cfe74161de68d4
james@crenexi.com <james@crenexi.com>
2020-04-13 11:27:40 -0700

    Added blend prop to BgCover

--- src/components/shared/BgCover/BgCover.jsx
+++ src/components/shared/BgCover/BgCover.jsx
@@ -1,10 +1,17 @@
 import React, { Fragment } from 'react';
-import PropTypes from 'prop-types';
+import PropTypes, { shape } from 'prop-types';
 import classNames from 'classnames';
 import './BgCover.scss';
 
 const BgCover = (props) => {
-  const { url, imagePosition, blanketColor, animate, children } = props;
+  const {
+    url,
+    imagePosition,
+    blanketColor,
+    blend,
+    animate,
+    children,
+  } = props;
 
   const imageClass = classNames('frame__cover-image', {
     'frame__cover-image--animate': animate,
@@ -19,6 +26,11 @@ const BgCover = (props) => {
     backgroundColor: blanketColor,
   };
 
+  const blendStyle = !blend ? null : {
+    backgroundColor: blend.color,
+    mixBlendMode: blend.mode,
+  };
+
   return (
     <Fragment>
       <div styleName="frame">
@@ -28,6 +40,9 @@ const BgCover = (props) => {
             style={imageStyle}
           />
           <div styleName="frame__cover-blanket" style={blanketStyle} />
+          { blend && (
+            <div styleName="frame__cover-blend" style={blendStyle} />
+          )}
         </div>
       </div>
       <div styleName="frame-content">{children}</div>
@@ -40,12 +55,17 @@ BgCover.propTypes = {
   url: PropTypes.string.isRequired,
   imagePosition: PropTypes.string,
   blanketColor: PropTypes.string,
+  blend: shape({
+    color: PropTypes.string.isRequired,
+    mode: PropTypes.string.isRequired,
+  }),
   animate: PropTypes.bool,
 };
 
 BgCover.defaultProps = {
   imagePosition: 'center center',
   blanketColor: 'rgba(0, 0, 0, .2)',
+  blend: null,
   animate: false,
 };
 
