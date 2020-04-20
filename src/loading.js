9cdc7418a6da8fc785a6a93f18b7235e51fffec7
james@crenexi.com <james@crenexi.com>
2020-04-19 23:27:32 -0700

    Added loading screen before root renders.

---
+++ src/loading.js
@@ -0,0 +1,32 @@
+import { icon } from '@fortawesome/fontawesome-svg-core';
+import { faStarOfLife } from '@fortawesome/pro-solid-svg-icons';
+import { faSpinnerThird } from '@fortawesome/pro-light-svg-icons';
+import './loading.scss';
+
+const onDocumentReady = () => {
+  const ids = {
+    star: 'root-loading-star',
+    spinner: 'root-loading-spinner',
+  };
+
+  // Star of life icon
+  (() => {
+    const elm = document.getElementById(ids.star);
+    const node = icon(faStarOfLife).node[0];
+    elm.appendChild(node);
+  })();
+
+  // Spinner icon
+  (() => {
+    const elm = document.getElementById(ids.spinner);
+    const node = icon(faSpinnerThird, {
+      classes: ['fa-spin'],
+      styles: {
+        'animation-duration': '350ms',
+      },
+    }).node[0];
+    elm.appendChild(node);
+  })();
+};
+
+document.addEventListener('DOMContentLoaded', onDocumentReady);
--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import './loading';
 
 // Set debug in localStorage
 if (process.env.NODE_ENV !== 'production') {
